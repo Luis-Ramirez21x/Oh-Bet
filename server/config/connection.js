9425cd0a3de0304d7297fa369ce68aa48362b625
Luis Ramirez <luis.ramirez21x@outlook.com>
2022-07-28 01:24:36 -0400

    forgot to push config file, api is still live and user is able to be created

---
+++ server/config/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/OhBetDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  
+});
+
+module.exports = mongoose.connection;

4c720ff4d753fa6c11c9fe8723e37d1de5bfdf25
Luis Ramirez <luis.ramirez21x@outlook.com>
2022-09-22 23:52:09 -0400

    server config for deployment

--- server/server.js
+++ server/server.js
@@ -16,6 +16,14 @@ app.use(cors({
 }))
 app.use(routes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port localhost:${PORT}!`);
